c54ac55e75da7a133c8e3c275e6163e1d3ee5727
saleek <saleek.pa@gmail.com>
2023-08-06 19:38:51 +0530

    Consolidated into one component

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -1,54 +1,64 @@
-import React from "react";
-import AddTask from "./AddTask";
-import ListTask from "./ListTask";
-import { useState } from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
-
+import React, { useState } from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { BsTrash } from "react-icons/bs";
 import "./ToDo.css";
 
 const ToDo = () => {
+  const [value, setValue] = useState("");
   const [tasks, setTasks] = useState([
-    {title: "Task 1"},
-    {title: "Task 2"}
+    { title: "Task 1" },
+    { title: "Task 2" },
   ]);
 
-  const addTask = (title) => {
-    const newTask = [...tasks, { title }];
-    setTasks(newTask);
+  const addItem = () => {
+    value.trim() !== "" && setTasks([...tasks, { title: value }]);
+    setValue("");
   };
 
   const deleteTask = (index) => {
-    const newTask = [...tasks];
-    newTask.splice(index, 1);
-    setTasks(newTask);
+    setTasks(tasks.filter((task) => tasks.indexOf(task) !== index));
   };
+
   return (
     <>
-        <Navbar bg="dark" data-bs-theme="dark">
-          <Container>
-            <Navbar.Brand href="#home">ToDo</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">About</Nav.Link>
-              <Nav.Link href="#pricing">Contact</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">ToDo</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">About</Nav.Link>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
       <div className="todo-container">
         <div className="header">ToDo App</div>
-        <div className="add-task">
-          <AddTask addTask={addTask} />
-        </div>
-        <div className="tasks">
-          {tasks.map((task, index) => (
-            <ListTask
-              task={task}
-              deleteTask={deleteTask}
-              index={index}
-              key={index}
+        <>
+          <div className="input-container">
+            <input
+              type="text"
+              className="input"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              placeholder="Add New Task"
             />
+            <Button variant="light" onClick={addItem}>
+              ADD
+            </Button>
+          </div>
+          {tasks.map((task, index) => (
+            <div className="list-task" key={index}>
+              {task.title}
+              <Button
+                variant="danger"
+                className="del-button"
+                onClick={() => deleteTask(index)}
+              >
+                <BsTrash />
+              </Button>
+            </div>
           ))}
-        </div>
+        </>
       </div>
     </>
   );
